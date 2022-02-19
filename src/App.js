17f88c64b2535775eb1e79428dc7a5ab789bf2b8
Brendan Dagys <brendandagys@gmail.com>
2022-02-19 10:26:35 -0500

    Added a new line

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
-          Edit <code>src/App.js</code> and save to reload. I WAS CHANGED AGAIN.
+          Edit <code>src/App.js</code> and save to reload!!! I WAS CHANGED.
         </p>
+        <p>Another added 'test' line</p>
         <a
           className='App-link'
           href='https://reactjs.org'
